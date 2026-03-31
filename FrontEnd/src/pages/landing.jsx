698c4ae356c5f43f9ea6ecc39b1493f54e5545e2
Vaishali Tyagi <37874212+vaishalityagi@users.noreply.github.com>
2026-03-31 12:41:54 +0530

    Update landing.jsx

--- FrontEnd/src/pages/landing.jsx
+++ FrontEnd/src/pages/landing.jsx
@@ -34,7 +34,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Cover a distance by my Video Call app</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
