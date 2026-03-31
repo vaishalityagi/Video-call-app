3555ed2d7a47e7d1d4fba179e0c93a18405ebbf2
Vaishali Tyagi <37874212+vaishalityagi@users.noreply.github.com>
2026-03-31 12:41:33 +0530

    Update landing.jsx

--- FrontEnd/src/pages/landing.jsx
+++ FrontEnd/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>My Video Call App</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
